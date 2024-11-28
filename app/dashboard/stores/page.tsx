3052aa4bbac7246e20059fae0d01df8a551649fa
ndela-ntu <ntulilindelani4@gmail.com>
2024-11-28 20:33:00 +0200

    saved

--- app/dashboard/stores/page.tsx
+++ app/dashboard/stores/page.tsx
@@ -1,8 +1,18 @@
 import Divider from "@/components/divider";
+import { createClient } from "@/utils/supabase/server";
 import { Plus } from "lucide-react";
 import Link from "next/link";
 
 export default async function Page() {
+  const supabase = createClient();
+  const { data: stores, error } = await (await supabase)
+    .from("stores")
+    .select(`*, coordinates (lat, lng)`);
+
+  if (error) {
+    return <div>{`An error occurred: ${error.message}`}</div>;
+  }
+  
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <div className="flex justify-between items-center w-full">
